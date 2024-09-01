4afe0c0eaa0b678a2ee29d9a0ba10a93bb09bfc7
Mystic <mym0404@gmail.com>
2024-09-01 23:06:39 +0900

    Release 0.0.25

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ export * from './src/hook/useStableCallback';
 export * from './src/hook/useUnmount';
 export * from './src/hook/useIsClient';
 export * from './src/hook/useLifecycle';
+export * from './src/hook/useRefValue';
 export * from './src/component/IntervalHandler';
 export * from './src/ticker/Ticker';
 export * from './src/ticker/useTicker';
---
+++ src/hook/useRefValue.ts
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+export function useRefValue<T>(init: () => T): T {
+  const [ref] = useState(() => init());
+
+  return ref;
+}
